8054e11da215c7ecaeefafdc1ce99e6c7c4b0778
vantaclaus <myersjac070@gmail.com>
2015-05-06 09:24:32 -0700

    Added deck construction

--- Deck.java
+++ Deck.java
@@ -5,8 +5,14 @@ public class Deck {
    private Random rng = new Random();
 
    public Deck(){
+      String[] suits = {"h","s","d","c"};
       deck = new ArrayList<Card>();
       //TODO: Add a card (2-14) of each suit (h,s,d,c)
+      for(int i=0; i<4; i++){
+        for(int j=2; j<15; j++){
+          deck.add(new Card(j,suits[i]));
+        }
+      }
    }
 
    public Card remove(){
